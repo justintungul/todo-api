e149644f305e3f033261ba50e66986f5199d216e
Justin Tungul <justin.tungul@gmail.com>
2017-06-22 18:41:02 +0800

    Added search for completed tasks

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET from url /todos
+// GET /todos -- GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
-// GET from url /todos/:id
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
